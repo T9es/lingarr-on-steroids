8c0a4f5e53672dc4774fcf516884dcd09c279a62
Yes <tomeov1@gmail.com>
2025-12-16 23:53:56 +0100

    fix(debug): log translation_schedule to debug spam

--- Lingarr.Server/Services/ScheduleService.cs
+++ Lingarr.Server/Services/ScheduleService.cs
@@ -61,6 +61,7 @@ public class ScheduleService : IScheduleService
                     if (setting.Value == "true")
                     {
                         var translationSchedule = await settingService.GetSetting("translation_schedule");
+                        _logger.LogError("AutomatedTranslationJob schedule: '{Schedule}'", translationSchedule);
                         RecurringJob.AddOrUpdate<AutomatedTranslationJob>(
                             "AutomatedTranslationJob",
                             job => job.Execute(),
